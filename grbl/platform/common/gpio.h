52372e27ae772b2243d9ae99dc7dfe1aa0829726
kimstik <kimstik@github.com>
2025-11-21 17:29:37 +0100

    Revert "Fix AVR build: skip common/gpio.h for AVR platform"
    This reverts commit e0c05ddde1be1b7b5ff1e65a4ccf5bc1f757fc05.

--- grbl/platform/common/gpio.h
+++ grbl/platform/common/gpio.h
@@ -1,5 +1,5 @@
 ﻿/*
-  gpio.h - GPIO abstraction for ARM platforms
+  gpio.h - GPIO abstraction for ALL platforms
   Part of Grbl
 
   Copyright (c) 2025 kimstik
@@ -7,15 +7,10 @@
   License: MIT
 
   NOTE: This file is auto-included via -include flag in Makefile
-  For AVR: Skip this file entirely (uses hal_gpio.h definitions)
-  For ARM: Provides GPIO abstraction macros
 */
 
 #ifndef GPIO_H
 #define GPIO_H
-
-// Skip this file for AVR - it has its own definitions in hal_gpio.h
-#ifndef __AVR__
 
 // -- generic part --
 #define BIT_MSK(nbit)		( 1<<(nbit) )
@@ -28,64 +23,81 @@
 #define BIT_CLR( x, nbit)	{ x &= ~BIT_MSK(nbit); }
 #define BIT_TGL(x, nbit)	{ x ^=  BIT_MSK(nbit); }
 
-//--  internal part - register definitions for ARM platforms --
+// -- temporal workaround part --
 
-#if !defined(GPIO_OUT_REG)
- #define GPIO_OUT_REG(name)	name##_PORT
+#ifdef __AVR__	// in the far future it have to be dropped. keep it for the momemt for initial integritiy check
+
+	// Port manipulation - CRITICAL: These must be identical to original GRBL!
+	// Write multiple pins atomically with mask
+	#define GPIO_WRITE_PORT(port, mask, value)	((port) = ((port) & ~(mask)) | ((value) & (mask)))
+
+	// Write single pin
+	#define GPIO_WRITE_PIN(port, pin, value) \
+	do { \
+	  if (value) \
+	    (port) |= (1 << (pin)); \
+	  else \
+	    (port) &= ~(1 << (pin)); \
+	} while(0)
+
 #endif
 
-#if !defined(GPIO_INP_REG)
- #define GPIO_INP_REG(name)	name##_PIN	// GPIO pin read (LIMIT/CONTROL/PROBE)	AVR strange naming used _PIN as data input reg... lets survive on this condition...
+//--  internal part --
+
+#if !defined(GPIO_OREG)
+ #define GPIO_OREG(name)	name##_PORT
 #endif
 
-#if !defined(GPIO_DIR_REG)
- #define GPIO_DIR_REG(name)	name##_DDR	// AVR-specific naming - subject to be redefined in platform
+#if !defined(GPIO_IREG)
+ #define GPIO_IREG(name)	name##_PIN	// GPIO pin read (LIMIT/CONTROL/PROBE)	AVR strange naming used _PIN as data input reg... lets survive on this condition...
 #endif
 
-#if !defined(GPIO_PU_REG)
- #define GPIO_PU_REG(name)	name##_PORT // AVR-specific - default
+#if !defined(GPIO_DREG)
+ #define GPIO_DREG(name)	name##_DDR	// AVR-specific naming - subject to be redefined in platform
+#endif
+
+#if !defined(GPIO_PREG)
+ #define GPIO_PREG(name)	name##_PORT // AVR-specific - default
 #endif
 
 
 // __scratch__, keepme
-//#define GPIO_PSET(port, val)  { GPIO_OUT_REG(port) = (val); }				// GPIO port write
-//#define GPIO_PGET(port) 		( GPIO_INP_REG(port) )						// GPIO port read
+//#define GPIO_PSET(port, val)  { GPIO_OREG(port) = (val); }				// GPIO port write
+//#define GPIO_PGET(port) 		( GPIO_IREG(port) )						// GPIO port read
 
 //-- finally usefull part - have to be used in GRBL base core mostly --
 // platforms may redefine them also in very flexible way by cherry-picking
 
 #if !defined(GPIO_BSET)
- #define GPIO_BSET(name)  		BIT_SET( GPIO_OUT_REG(name), name##_BIT )	// Set    gpio pin ( = 1)
+ #define GPIO_BSET(name)  		BIT_SET( GPIO_OREG(name), name##_BIT )	// Set    gpio pin ( = 1)
 #endif
 
 #if !defined(GPIO_BCLR)
- #define GPIO_BCLR(name)  		BIT_CLR( GPIO_OUT_REG(name), name##_BIT )	// Clear  gpio pin ( = 0)
+ #define GPIO_BCLR(name)  		BIT_CLR( GPIO_OREG(name), name##_BIT )	// Clear  gpio pin ( = 0)
 #endif
 
 #if !defined(GPIO_BTGL)
- #define GPIO_BTGL(name)  		BIT_TGL( GPIO_OUT_REG(name), name##_BIT )	// Toggle gpio pin
+ #define GPIO_BTGL(name)  		BIT_TGL( GPIO_OREG(name), name##_BIT )	// Toggle gpio pin
 #endif
 
 #if !defined(GPIO_DIR_OUT)
- #define GPIO_DIR_OUT(name)  	BIT_SET( GPIO_DIR_REG(name), name##_BIT )	// Set gpio pin as output
+ #define GPIO_DIR_OUT(name)  	BIT_SET( GPIO_DREG(name), name##_BIT )	// Set gpio pin as output
 #endif
 
 #if !defined(GPIO_DIR_INP)
- #define GPIO_DIR_INP(name)  	BIT_CLR( GPIO_DIR_REG(name), name##_BIT )	// Set gpio pin as input
+ #define GPIO_DIR_INP(name)  	BIT_CLR( GPIO_DREG(name), name##_BIT )	// Set gpio pin as input
 #endif
 
 #if !defined(GPIO_PULLUP_EN)
- #define GPIO_PULLUP_EN( name)	BIT_SET( GPIO_PU_REG(name),  name##_BIT )	// GPIO pin pull-up enable
+ #define GPIO_PULLUP_EN( name)	BIT_SET( GPIO_PREG(name),  name##_BIT )	// GPIO pin pull-up enable
 #endif
 
 #if !defined(GPIO_PULLUP_DIS)
- #define GPIO_PULLUP_DIS(name)	BIT_CLR( GPIO_PU_REG(name),  name##_BIT )
+ #define GPIO_PULLUP_DIS(name)	BIT_CLR( GPIO_PREG(name),  name##_BIT )
 #endif
 
 #if !defined(GPIO_BGET)
- #define GPIO_BGET(name) 	 	( !(GPIO_INP_REG(name) & BIT_MSK(name##_BIT)) != 0 )	// GPIO pin get/read (LIMIT/CONTROL/PROBE)
+ #define GPIO_BGET(name) 	 	( !(GPIO_IREG(name) & BIT_MSK(name##_BIT)) != 0 )	// GPIO pin get/read (LIMIT/CONTROL/PROBE)
 #endif
-
-#endif // !__AVR__
 
 #endif // GPIO_H
--- grbl/platform/samd21/gpio.h
+++ grbl/platform/samd21/gpio.h
@@ -9,9 +9,9 @@
 
 // platform specific notation - to be defined before platform\common\gpio.h 
 
-#define GPIO_OUT_REG(name)	PORT->Group[name##_PORT].OUT	// GPIO output register - to write to                         
-#define GPIO_INP_REG(name)	PORT->Group[name##_PORT].IN		// GPIO input register  - tp read from (LIMIT/CONTROL/PROBE)  
-#define GPIO_DIR_REG(name)	PORT->Group[name##_PORT].DIR    // GPIO direction control reg                                 
-#define GPIO_PU_REG(name)	PORT->Group[name##_PORT].CTRL   // GPIO pullup control                                        
+#define GPIO_OREG(name)	PORT->Group[name##_PORT].OUT	// GPIO output register - to write to                         
+#define GPIO_IREG(name)	PORT->Group[name##_PORT].IN		// GPIO input register  - tp read from (LIMIT/CONTROL/PROBE)  
+#define GPIO_DREG(name)	PORT->Group[name##_PORT].DIR    // GPIO direction control reg                                 
+#define GPIO_PREG(name)	PORT->Group[name##_PORT].CTRL   // GPIO pullup control                                        
 
 #endif // GPIO_SAMD_H
